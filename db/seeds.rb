e8cef3ee762a84148cfa99d9a9dec44935d68033
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-11 13:24:24 +0000

    Seeded the database with 5 restaurants with 2 reviews each

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying Database"
+Restaurant.destroy_all
+
+5.times do
+  puts "Creating Restaurant"
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  restaurant.save
+
+  2.times do
+    puts "Creating Review"
+    review = Review.new(rating: rand(0..5), content: Faker::Restaurant.review, restaurant_id: restaurant.id )
+    review.save
+  end
+end
