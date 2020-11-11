979cd74a5e7df729d54c8451053a0c03a76bed69
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-11 15:39:33 +0000

    Adding review infrastructure

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,12 +4,21 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(restaurant.id)
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
